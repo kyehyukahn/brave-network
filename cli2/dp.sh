818a1565deb410fff9ab6b27c1b112630c2bd901
alwayswinners <kyehyuk.ahn@blockchainos.org>
2018-06-22 16:37:08 +0900

    add 1b1n and 1u1n

--- cli2/dp.sh
+++
@@ -1,6 +0,0 @@
-SEED=`bin/brave keypair generate | awk '{print $2}'`
-ADDRESS=`bin/brave keypair parse --seed $SEED | awk '{print $2}'`
-
-bin/brave transaction fund --seed $ROOT_SEED \
-    --address $ADDRESS \
-    --amount 10000
